2cae667baa8beeda3d565add53a0c6d387c46c0e
Шувалов-Лужин Матвей <matveyshuvaloff@yandex.ru>
2021-11-09 22:17:05 +0300

    upd

--- src/ru/mirea/task12/Test.java
+++ src/ru/mirea/task12/Test.java
@@ -5,24 +5,35 @@ import java.util.Comparator;
 
 public class Test {
 
-    public Test(){}
-
-    public Student[] array = new Student[5];
-
-    public void fill_array(){
-        array[0] = new Student((int)(Math.random() * 50), "Bober", (int)(Math.random() * 5));
-        array[1] = new Student((int)(Math.random() * 50), "Osel",  (int)(Math.random() * 5));
-        array[2] = new Student((int)(Math.random() * 50), "Kozel", (int)(Math.random() * 5));
-        array[3] = new Student((int)(Math.random() * 50), "Baran", (int)(Math.random() * 5));
-        array[4] = new Student((int)(Math.random() * 50), "Petuh", (int)(Math.random() * 5));
+    public Test() {
     }
 
-    public void print_array(){
+    public Student[] array = new Student[5];
+    public Student[] temp_array = new Student[5];
+
+    public void fill_array() {
+        array[0] = new Student((int) (Math.random() * 50), "Bober", (int) (Math.random() * 5));
+        array[1] = new Student((int) (Math.random() * 50), "Osel", (int) (Math.random() * 5));
+        array[2] = new Student((int) (Math.random() * 50), "Kozel", (int) (Math.random() * 5));
+        array[3] = new Student((int) (Math.random() * 50), "Baran", (int) (Math.random() * 5));
+        array[4] = new Student((int) (Math.random() * 50), "Petuh", (int) (Math.random() * 5));
+    }
+
+    public void fill_temp_array() {
+        array[0] = new Student((int) (Math.random() * 50), "Ovca", (int) (Math.random() * 5));
+        array[1] = new Student((int) (Math.random() * 50), "Medved", (int) (Math.random() * 5));
+        array[2] = new Student((int) (Math.random() * 50), "Obezyana", (int) (Math.random() * 5));
+        array[3] = new Student((int) (Math.random() * 50), "Jiraf", (int) (Math.random() * 5));
+        array[4] = new Student((int) (Math.random() * 50), "Koala", (int) (Math.random() * 5));
+    }
+
+
+    public void print_array() {
         for (int i = 0; i < 5; i++) System.out.println(array[i].toString());
         System.out.println("");
     }
 
-    public void insert_sort_id(){
+    public void insert_sort_id() {
         for (int left = 0; left < array.length; left++) {
             int temp_id = array[left].id;
             String temp_name = array[left].name;
@@ -44,10 +55,12 @@ public class Test {
         }
     }
 
-    public void quick_sort(){
+    public void quick_sort() {
         Comparator cmpr = new SortingStudentsByGPA();
-        Arrays.sort(array,cmpr);
+        Arrays.sort(array, cmpr);
     }
+
+
 
     public static void main(String[] args) {
         Test run = new Test();
@@ -57,5 +70,6 @@ public class Test {
         run.print_array();
         run.quick_sort();
         run.print_array();
+        run.fill_temp_array();
     }
 }
